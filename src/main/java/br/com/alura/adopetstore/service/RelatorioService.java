0bf1b9389e6614076ba010f055aee09f74ce872c
Caio Amaro <caioamarocci@gmail.com>
2025-06-08 01:32:09 -0300

    restrutura

--- src/main/java/br/com/alura/adopetstore/service/RelatorioService.java
+++ src/main/java/br/com/alura/adopetstore/service/RelatorioService.java
@@ -7,11 +7,9 @@ import br.com.alura.adopetstore.repository.EstoqueRepository;
 import br.com.alura.adopetstore.repository.PedidoRepository;
 import br.com.alura.adopetstore.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 @Service
@@ -25,21 +23,19 @@ public class RelatorioService {
     @Autowired
     private PedidoRepository pedidoRepository;
 
-    @Async
-    public CompletableFuture<RelatorioEstoque> infoEstoque(){
+    public RelatorioEstoque infoEstoque(){
         var produtosSemEstoque = estoqueRepository.produtosComEstoqueZerado()
                 .stream().map(ProdutoDTO::new)
                 .collect(Collectors.toList());
-        return CompletableFuture.completedFuture(new RelatorioEstoque(produtosSemEstoque));
+        return new RelatorioEstoque(produtosSemEstoque);
     }
 
-    @Async
-    public CompletableFuture<RelatorioFaturamento> faturamentoObtido() {
+    public RelatorioFaturamento faturamentoObtido() {
         var dataOntem = LocalDate.now().minusDays(1);
         var faturamentoTotal = pedidoRepository.faturamentoTotalDoDia(dataOntem);
 
         var estatisticas = pedidoRepository.faturamentoTotalDoDiaPorCategoria(dataOntem);
 
-        return CompletableFuture.completedFuture(new RelatorioFaturamento(faturamentoTotal, estatisticas));
+        return new RelatorioFaturamento(faturamentoTotal, estatisticas);
     }
 }
